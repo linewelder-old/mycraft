7e00269c2f421522c6028cc9f2320a09948e6ca1
linewelder <redalerm@gmail.com>
2023-02-18 15:40:49 +0100

    Adjust constants
    - Make window bigger
    - Make camera move faster

--- src/consts.rs
+++ src/consts.rs
@@ -1,6 +1,6 @@
 pub const WIN_TITLE: &str = "Mycraft";
-pub const WIN_SIZE: (u32, u32) = (800, 600);
+pub const WIN_SIZE: (u32, u32) = (1080, 720);
 pub const FPS: u32 = 60;
 
-pub const CAMERA_MOVEMENT_SPEED: f32 = 2.;
+pub const CAMERA_MOVEMENT_SPEED: f32 = 4.;
 pub const MOUSE_SENSITIVITY: f32 = 0.2;
