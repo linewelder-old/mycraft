128f18a2cbd4c7e769a681ff8163e74a3da1d378
linewelder <redalerm@gmail.com>
2023-02-19 19:32:52 +0100

    Add World to handle chunks

--- src/world.rs
+++ src/world.rs
@@ -1,10 +1,26 @@
 pub mod generation;
 pub mod mesh;
 
-use cgmath::Vector3;
+use std::collections::HashMap;
+
+use cgmath::{Matrix4, Vector2, Vector3};
+
+use crate::{
+    context::Context,
+    rendering::{block_renderer::Vertex, uniform::Uniform, vertex_array::VertexArray},
+    world::{generation::generate_chunk, mesh::generate_chunk_mesh},
+};
+
+pub struct ChunkGraphics {
+    pub mesh: VertexArray<Vertex>,
+    pub transform: Uniform<Matrix4<f32>>,
+}
 
 pub struct Chunk {
     pub blocks: [[[bool; Self::SIZE.z]; Self::SIZE.y]; Self::SIZE.x],
+
+    pub graphics: Option<ChunkGraphics>,
+    needs_update: bool,
 }
 
 impl Chunk {
@@ -16,7 +32,53 @@ impl Chunk {
 
     pub fn new() -> Self {
         Chunk {
-            blocks: [[[false; Self::SIZE.z]; Self::SIZE.y]; Self::SIZE.x]
+            blocks: [[[false; Self::SIZE.z]; Self::SIZE.y]; Self::SIZE.x],
+            graphics: None,
+            needs_update: true,
+        }
+    }
+}
+
+pub type ChunkCoords = Vector2<i32>;
+
+pub struct World {
+    pub chunks: HashMap<ChunkCoords, Chunk>,
+}
+
+impl World {
+    pub fn new() -> Self {
+        World {
+            chunks: HashMap::new(),
+        }
+    }
+
+    pub fn load_chunk(&mut self, coords: ChunkCoords) {
+        if self.chunks.contains_key(&coords) {
+            return;
+        }
+
+        let mut chunk = Chunk::new();
+        generate_chunk(&mut chunk, coords);
+        self.chunks.insert(coords, chunk);
+    }
+
+    pub fn update_chunk_graphics(&mut self, context: &Context) {
+        for (coords, chunk) in &mut self.chunks {
+            if chunk.needs_update {
+                let translation = Vector3 {
+                    x: coords.x as f32 * Chunk::SIZE.x as f32,
+                    y: 0.,
+                    z: coords.y as f32 * Chunk::SIZE.z as f32,
+                };
+
+                let mesh = generate_chunk_mesh(context, &chunk);
+                let transform = Uniform::new(
+                    context,
+                    "Chunk Transform",
+                    Matrix4::from_translation(translation),
+                );
+                chunk.graphics = Some(ChunkGraphics { mesh, transform });
+            }
         }
     }
 }
--- src/world/generation.rs
+++ src/world/generation.rs
@@ -1,6 +1,6 @@
-use crate::world::Chunk;
+use crate::world::{Chunk, ChunkCoords};
 
-pub fn generate_chunk(chunk: &mut Chunk) {
+pub fn generate_chunk(chunk: &mut Chunk, _chunk_coords: ChunkCoords) {
     for x in 0..Chunk::SIZE.x {
         for z in 0..Chunk::SIZE.z {
             for y in 0..3 {
