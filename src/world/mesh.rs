d258c17a7c4c49a44f7cfccbc28c131336945a2f
linewelder <redalerm@gmail.com>
2023-02-21 21:40:21 +0100

    Refactor mesh generation
    Create MeshGenerationContext

--- src/world/mesh.rs
+++ src/world/mesh.rs
@@ -3,7 +3,10 @@ use cgmath::{Vector2, Vector3};
 use crate::{
     context::Context,
     rendering::{chunk_renderer::Vertex, vertex_array::VertexArray},
-    world::{blocks::BLOCKS, Chunk, ChunkCoords, World},
+    world::{
+        blocks::{Block, BLOCKS},
+        Chunk, ChunkCoords, World,
+    },
 };
 
 #[rustfmt::skip]
@@ -69,47 +72,33 @@ const TEX_COORDS: [Vector2<f32>; 4] = [
     Vector2 { x: 1., y: 0. },
 ];
 
-fn emit_face(vertices: &mut Vec<Vertex>, i: usize, texture_id: u32, offset: Vector3<f32>) {
-    let base_texture_coords = Vector2 {
-        x: (texture_id % 4) as f32,
-        y: (texture_id / 4) as f32,
-    };
-
-    FACES[i]
-        .iter()
-        .zip(TEX_COORDS)
-        .map(|(&pos, tex)| Vertex {
-            pos: pos + offset,
-            tex: (base_texture_coords + tex) / 4.,
-            normal: NEIGHBOR_OFFSETS[i].map(|x| x as f32),
-        })
-        .for_each(|x| vertices.push(x));
-
-    // Split into triangles
-    vertices.insert(vertices.len() - 1, vertices[vertices.len() - 2]);
-    vertices.insert(vertices.len() - 1, vertices[vertices.len() - 4]);
+struct MeshGenerationContext<'a> {
+    neighbor_chunks: [[Option<&'a Chunk>; 3]; 3],
+    vertices: Vec<Vertex>,
 }
 
-pub fn generate_chunk_mesh(
-    context: &Context,
-    world: &World,
-    chunk: &Chunk,
-    chunk_coords: ChunkCoords,
-) -> VertexArray<Vertex> {
-    // Cache neighboring chunks
-    let mut chunks: [[Option<&Chunk>; 3]; 3] = [[None; 3]; 3];
-    for x in 0..3 {
-        for y in 0..3 {
-            let offset = ChunkCoords {
-                x: x as i32 - 1,
-                y: y as i32 - 1,
-            };
-            chunks[x][y] = world.chunks.get(&(chunk_coords + offset));
+impl<'a> MeshGenerationContext<'a> {
+    fn new(world: &'a World, chunk_coords: ChunkCoords) -> Self {
+        // Cache neighboring chunks
+        let mut neighbor_chunks: [[Option<&Chunk>; 3]; 3] = [[None; 3]; 3];
+        for x in 0..3 {
+            for y in 0..3 {
+                let offset = ChunkCoords {
+                    x: x as i32 - 1,
+                    y: y as i32 - 1,
+                };
+                neighbor_chunks[x][y] = world.chunks.get(&(chunk_coords + offset));
+            }
+        }
+
+        MeshGenerationContext {
+            neighbor_chunks,
+            vertices: vec![],
         }
     }
 
     // Coords are relative to middle chunk in chunks array
-    fn is_transparent(chunks: &[[Option<&Chunk>; 3]; 3], coords: Vector3<i32>) -> bool {
+    fn is_transparent(&self, coords: Vector3<i32>) -> bool {
         if coords.y > Chunk::SIZE.y as i32 || coords.y < 0 {
             return true;
         }
@@ -120,7 +109,7 @@ pub fn generate_chunk_mesh(
 
         let chunk_x = relative_x / Chunk::SIZE.x;
         let chunk_z = relative_z / Chunk::SIZE.z;
-        let chunk = chunks[chunk_x][chunk_z];
+        let chunk = self.neighbor_chunks[chunk_x][chunk_z];
 
         if let Some(chunk) = chunk {
             let block_x = relative_x % Chunk::SIZE.x;
@@ -133,7 +122,58 @@ pub fn generate_chunk_mesh(
         }
     }
 
-    let mut vertices = vec![];
+    fn emit_face(&mut self, i: usize, texture_id: u32, offset: Vector3<f32>) {
+        let base_texture_coords = Vector2 {
+            x: (texture_id % 4) as f32,
+            y: (texture_id / 4) as f32,
+        };
+
+        FACES[i]
+            .iter()
+            .zip(TEX_COORDS)
+            .map(|(&pos, tex)| Vertex {
+                pos: pos + offset,
+                tex: (base_texture_coords + tex) / 4.,
+                normal: NEIGHBOR_OFFSETS[i].map(|x| x as f32),
+            })
+            .for_each(|x| self.vertices.push(x));
+
+        // Split into triangles
+        self.vertices.insert(
+            self.vertices.len() - 1,
+            self.vertices[self.vertices.len() - 2],
+        );
+        self.vertices.insert(
+            self.vertices.len() - 1,
+            self.vertices[self.vertices.len() - 4],
+        );
+    }
+
+    fn emit_block(&mut self, x: i32, y: i32, z: i32, block: &Block) {
+        if let Some(textures) = &block.texture_ids {
+            let block_offset = Vector3 {
+                x: x as f32,
+                y: y as f32,
+                z: z as f32,
+            };
+
+            for (i, neighbor_offset) in NEIGHBOR_OFFSETS.iter().enumerate() {
+                let neighbor_coords = Vector3 { x, y, z } + neighbor_offset;
+                if self.is_transparent(neighbor_coords) {
+                    self.emit_face(i, textures[i], block_offset);
+                }
+            }
+        }
+    }
+}
+
+pub fn generate_chunk_mesh(
+    context: &Context,
+    world: &World,
+    chunk: &Chunk,
+    chunk_coords: ChunkCoords,
+) -> VertexArray<Vertex> {
+    let mut generation_context = MeshGenerationContext::new(world, chunk_coords);
 
     for x in 0..Chunk::SIZE.x as i32 {
         for y in 0..Chunk::SIZE.y as i32 {
@@ -141,23 +181,10 @@ pub fn generate_chunk_mesh(
                 let block_id = chunk.blocks[x as usize][y as usize][z as usize];
                 let block = &BLOCKS[block_id];
 
-                if let Some(textures) = &block.texture_ids {
-                    let block_offset = Vector3 {
-                        x: x as f32,
-                        y: y as f32,
-                        z: z as f32,
-                    };
-
-                    for (i, neighbor_offset) in NEIGHBOR_OFFSETS.iter().enumerate() {
-                        let neighbor_coords = Vector3 { x, y, z } + neighbor_offset;
-                        if is_transparent(&chunks, neighbor_coords) {
-                            emit_face(&mut vertices, i, textures[i], block_offset);
-                        }
-                    }
-                }
+                generation_context.emit_block(x, y, z, block);
             }
         }
     }
 
-    VertexArray::new(context, "Chunk Mesh", &vertices)
+    VertexArray::new(context, "Chunk Mesh", &generation_context.vertices)
 }
