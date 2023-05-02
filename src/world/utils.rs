255b72d0bf5d6bdfd2c529652da44a2ee7f4566d
linewelder <redalerm@gmail.com>
2023-05-02 16:27:19 +0200

    Make chunk neighborhoods aware that chunks are 3d

--- src/world/utils.rs
+++ src/world/utils.rs
@@ -27,101 +27,106 @@ pub fn to_chunk_offset(chunk_coords: ChunkCoords) -> Vector3<f32> {
     chunk_coords.mul_element_wise(Chunk::SIZE).map(|x| x as f32)
 }
 
-/// Borrows a 3x3 chunk region
+/// Borrows a 3x3x3 chunk region
 pub struct ChunkNeighborhood<'a> {
     chunk: &'a Chunk,
-    neighbors: [[Option<Ref<'a, Chunk>>; 3]; 3],
+    neighbors: [[[Option<Ref<'a, Chunk>>; 3]; 3]; 3],
+}
+
+const fn empty_neighbor_array<T>() -> [[[Option<T>; 3]; 3]; 3] {
+    [
+        [[None, None, None], [None, None, None], [None, None, None]],
+        [[None, None, None], [None, None, None], [None, None, None]],
+        [[None, None, None], [None, None, None], [None, None, None]],
+    ]
+}
+
+fn borrow_neighborhood<'a, T: 'a>(
+    world: &'a World,
+    chunk_coords: ChunkCoords,
+    f: impl Fn(&'a World, ChunkCoords) -> Option<T>,
+) -> [[[Option<T>; 3]; 3]; 3] {
+    let mut neighbors = empty_neighbor_array();
+    #[allow(clippy::needless_range_loop)]
+    for x in 0..3 {
+        for y in 0..3 {
+            for z in 0..3 {
+                if x != 1 || y != 1 || z != 1 {
+                    let offset = ChunkCoords {
+                        x: x as i32 - 1,
+                        y: y as i32 - 1,
+                        z: z as i32 - 1,
+                    };
+                    neighbors[x][y][z] = f(world, chunk_coords + offset);
+                }
+            }
+        }
+    }
+
+    neighbors
 }
 
 impl<'a> ChunkNeighborhood<'a> {
     pub fn new(world: &'a World, chunk: &'a Chunk, chunk_coords: ChunkCoords) -> Self {
-        let mut neighbors = [[None, None, None], [None, None, None], [None, None, None]];
-        #[allow(clippy::needless_range_loop)]
-        for x in 0..3 {
-            for y in 0..3 {
-                if x != 1 || y != 1 {
-                    let offset = ChunkCoords {
-                        x: x as i32 - 1,
-                        y: 0,
-                        z: y as i32 - 1,
-                    };
-                    neighbors[x][y] = world.borrow_chunk(chunk_coords + offset);
-                }
-            }
-        }
-
+        let neighbors = borrow_neighborhood(world, chunk_coords, World::borrow_chunk);
         ChunkNeighborhood { chunk, neighbors }
     }
 
     // Coords are relative to middle chunk in chunks array
     pub fn get_cell(&self, coords: Vector3<i32>) -> Option<Cell> {
-        if coords.y >= Chunk::SIZE.y || coords.y < 0 {
-            return None;
-        }
-
-        if coords.x >= 0 && coords.x < Chunk::SIZE.x && coords.z >= 0 && coords.z < Chunk::SIZE.z {
+        if (coords.x >= 0 && coords.x < Chunk::SIZE.x)
+            && (coords.y >= 0 && coords.y < Chunk::SIZE.y)
+            && (coords.z >= 0 && coords.z < Chunk::SIZE.z)
+        {
             return Some(self.chunk[coords]);
         }
 
         let (chunk_coords, block_coords) = to_local_chunk_coords(coords);
-        self.neighbors[(chunk_coords.x + 1) as usize][(chunk_coords.y + 1) as usize]
+        let array_coords = chunk_coords.map(|x| (x + 1) as usize);
+        self.neighbors[array_coords.x][array_coords.y][array_coords.z]
             .as_ref()
             .map(|chunk| chunk[block_coords])
     }
 }
 
-/// Borrows a 3x3 chunk region mutably
+/// Borrows a 3x3x3 chunk region mutably
 pub struct ChunkNeighborhoodMut<'a> {
     chunk: &'a mut Chunk,
-    neighbors: [[Option<RefMut<'a, Chunk>>; 3]; 3],
+    neighbors: [[[Option<RefMut<'a, Chunk>>; 3]; 3]; 3],
 }
 
 impl<'a> ChunkNeighborhoodMut<'a> {
     pub fn new(world: &'a World, chunk: &'a mut Chunk, chunk_coords: ChunkCoords) -> Self {
-        let mut neighbors = [[None, None, None], [None, None, None], [None, None, None]];
-        #[allow(clippy::needless_range_loop)]
-        for x in 0..3 {
-            for y in 0..3 {
-                if x != 1 || y != 1 {
-                    let offset = ChunkCoords {
-                        x: x as i32 - 1,
-                        y: 0,
-                        z: y as i32 - 1,
-                    };
-                    neighbors[x][y] = world.borrow_mut_chunk(chunk_coords + offset);
-                }
-            }
-        }
-
+        let neighbors = borrow_neighborhood(world, chunk_coords, World::borrow_mut_chunk);
         ChunkNeighborhoodMut { chunk, neighbors }
     }
 
     pub fn get_cell(&self, coords: Vector3<i32>) -> Option<Cell> {
-        if coords.y >= Chunk::SIZE.y || coords.y < 0 {
-            return None;
-        }
-
-        if coords.x >= 0 && coords.x < Chunk::SIZE.x && coords.z >= 0 && coords.z < Chunk::SIZE.z {
+        if (coords.x >= 0 && coords.x < Chunk::SIZE.x)
+            && (coords.y >= 0 && coords.y < Chunk::SIZE.y)
+            && (coords.z >= 0 && coords.z < Chunk::SIZE.z)
+        {
             return Some(self.chunk[coords]);
         }
 
         let (chunk_coords, block_coords) = to_local_chunk_coords(coords);
-        self.neighbors[(chunk_coords.x + 1) as usize][(chunk_coords.y + 1) as usize]
+        let array_coords = chunk_coords.map(|x| (x + 1) as usize);
+        self.neighbors[array_coords.x][array_coords.y][array_coords.z]
             .as_ref()
             .map(|chunk| chunk[block_coords])
     }
 
     pub fn get_cell_mut(&mut self, coords: Vector3<i32>) -> Option<&mut Cell> {
-        if coords.y >= Chunk::SIZE.y || coords.y < 0 {
-            return None;
-        }
-
-        if coords.x >= 0 && coords.x < Chunk::SIZE.x && coords.z >= 0 && coords.z < Chunk::SIZE.z {
+        if (coords.x >= 0 && coords.x < Chunk::SIZE.x)
+            && (coords.y >= 0 && coords.y < Chunk::SIZE.y)
+            && (coords.z >= 0 && coords.z < Chunk::SIZE.z)
+        {
             return Some(&mut self.chunk[coords]);
         }
 
         let (chunk_coords, block_coords) = to_local_chunk_coords(coords);
-        self.neighbors[(chunk_coords.x + 1) as usize][(chunk_coords.y + 1) as usize]
+        let array_coords = chunk_coords.map(|x| (x + 1) as usize);
+        self.neighbors[array_coords.x][array_coords.y][array_coords.z]
             .as_mut()
             .map(|chunk| &mut chunk[block_coords])
     }
