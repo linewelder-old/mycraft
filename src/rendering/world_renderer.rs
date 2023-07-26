e9d1cbdad2635ee2259ca2d22235b57510eb4447
linewelder <redalerm@gmail.com>
2023-07-26 10:23:37 +0200

    Make one function to create world render pipelines
    Removes code repetition.

--- src/rendering/world_renderer.rs
+++ src/rendering/world_renderer.rs
@@ -11,15 +11,35 @@ use super::{
 };
 
 pub struct WorldRenderer {
-    solid_block_pipeline: SolidBlockPipeline,
-    water_pipeline: WaterPipeline,
+    solid_block_pipeline: wgpu::RenderPipeline,
+    water_pipeline: wgpu::RenderPipeline,
     blocks_texture: Rc<Texture>,
 }
 
 impl WorldRenderer {
     pub fn new(context: &Context, blocks_texture: Rc<Texture>) -> Self {
-        let solid_block_pipeline = SolidBlockPipeline::new(context);
-        let water_pipeline = WaterPipeline::new(context);
+        let solid_block_pipeline = create_world_pipeline(
+            context,
+            WorldPipelineDesc {
+                label: "Solid Block Render Pipeline",
+                blend: wgpu::BlendState::REPLACE,
+                shader: wgpu::include_wgsl!("solid_block_shader.wgsl"),
+                cull_mode: Some(wgpu::Face::Back),
+                depth_write_enabled: true,
+            },
+        );
+
+        let water_pipeline = create_world_pipeline(
+            context,
+            WorldPipelineDesc {
+                label: "Water Block Render Pipeline",
+                blend: wgpu::BlendState::ALPHA_BLENDING,
+                shader: wgpu::include_wgsl!("water_shader.wgsl"),
+                cull_mode: None,
+                depth_write_enabled: false,
+            },
+        );
+
         WorldRenderer {
             solid_block_pipeline,
             water_pipeline,
@@ -55,100 +75,12 @@ impl WorldRenderer {
             }),
         });
 
-        self.solid_block_pipeline.draw(
-            &mut render_pass,
-            camera,
-            chunks.clone(),
-            sky,
-            &self.blocks_texture,
-        );
-        self.water_pipeline
-            .draw(&mut render_pass, camera, chunks, sky, &self.blocks_texture);
-    }
-}
-
-struct SolidBlockPipeline {
-    render_pipeline: wgpu::RenderPipeline,
-}
-
-impl SolidBlockPipeline {
-    pub fn new(context: &Context) -> Self {
-        let bind_group_layouts = &[
-            &Camera::create_bind_group_layout(context),
-            &Sky::create_bind_group_layout(context),
-            &Texture::create_bind_group_layout(context),
-            &Uniform::<Vector3<f32>>::create_bind_group_layout(context),
-        ];
-
-        let layout = context
-            .device
-            .create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
-                label: Some("Solid Block Render Pipeline Layout"),
-                bind_group_layouts,
-                push_constant_ranges: &[],
-            });
-
-        let shader = context
-            .device
-            .create_shader_module(wgpu::include_wgsl!("solid_block_shader.wgsl"));
-
-        let render_pipeline =
-            context
-                .device
-                .create_render_pipeline(&wgpu::RenderPipelineDescriptor {
-                    label: Some("Solid Block Render Pipeline"),
-                    layout: Some(&layout),
-                    vertex: wgpu::VertexState {
-                        module: &shader,
-                        entry_point: "vs_main",
-                        buffers: &[Vertex::BUFFER_LAYOUT],
-                    },
-                    fragment: Some(wgpu::FragmentState {
-                        module: &shader,
-                        entry_point: "fs_main",
-                        targets: &[Some(wgpu::ColorTargetState {
-                            format: context.surface_config.borrow().format,
-                            blend: Some(wgpu::BlendState::REPLACE),
-                            write_mask: wgpu::ColorWrites::ALL,
-                        })],
-                    }),
-                    primitive: wgpu::PrimitiveState {
-                        topology: wgpu::PrimitiveTopology::TriangleList,
-                        strip_index_format: None,
-                        front_face: wgpu::FrontFace::Ccw,
-                        cull_mode: Some(wgpu::Face::Back),
-                        polygon_mode: wgpu::PolygonMode::Fill,
-                        unclipped_depth: false,
-                        conservative: false,
-                    },
-                    depth_stencil: Some(wgpu::DepthStencilState {
-                        format: DepthBuffer::FORMAT,
-                        depth_write_enabled: true,
-                        depth_compare: wgpu::CompareFunction::Less,
-                        stencil: wgpu::StencilState::default(),
-                        bias: wgpu::DepthBiasState::default(),
-                    }),
-                    multisample: wgpu::MultisampleState::default(),
-                    multiview: None,
-                });
-
-        SolidBlockPipeline { render_pipeline }
-    }
-
-    pub fn draw<'a>(
-        &'a self,
-        render_pass: &mut wgpu::RenderPass<'a>,
-        camera: &'a Camera,
-        chunks: impl Iterator<Item = &'a ChunkGraphics>,
-        sky_uniform: &'a Sky,
-        texture: &'a Texture,
-    ) {
-        render_pass.set_pipeline(&self.render_pipeline);
+        render_pass.set_pipeline(&self.solid_block_pipeline);
         render_pass.set_bind_group(0, camera.get_bind_group(), &[]);
-        render_pass.set_bind_group(1, sky_uniform.get_bind_group(), &[]);
-        render_pass.set_bind_group(2, texture.get_bind_group(), &[]);
+        render_pass.set_bind_group(1, sky.get_bind_group(), &[]);
+        render_pass.set_bind_group(2, self.blocks_texture.get_bind_group(), &[]);
 
-        for chunk in chunks {
+        for chunk in chunks.clone() {
             render_pass.set_bind_group(3, chunk.offset.get_bind_group(), &[]);
 
             render_pass.set_vertex_buffer(0, chunk.solid_mesh.vertices.slice(..));
@@ -158,89 +90,8 @@ impl SolidBlockPipeline {
             );
             render_pass.draw_indexed(0..chunk.solid_mesh.index_count, 0, 0..1);
         }
-    }
-}
 
-struct WaterPipeline {
-    render_pipeline: wgpu::RenderPipeline,
-}
-
-impl WaterPipeline {
-    pub fn new(context: &Context) -> Self {
-        let bind_group_layouts = &[
-            &Camera::create_bind_group_layout(context),
-            &Sky::create_bind_group_layout(context),
-            &Texture::create_bind_group_layout(context),
-            &Uniform::<Vector3<f32>>::create_bind_group_layout(context),
-        ];
-
-        let layout = context
-            .device
-            .create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
-                label: Some("Water Render Pipeline Layout"),
-                bind_group_layouts,
-                push_constant_ranges: &[],
-            });
-
-        let shader = context
-            .device
-            .create_shader_module(wgpu::include_wgsl!("water_shader.wgsl"));
-
-        let render_pipeline =
-            context
-                .device
-                .create_render_pipeline(&wgpu::RenderPipelineDescriptor {
-                    label: Some("Water Render Pipeline"),
-                    layout: Some(&layout),
-                    vertex: wgpu::VertexState {
-                        module: &shader,
-                        entry_point: "vs_main",
-                        buffers: &[Vertex::BUFFER_LAYOUT],
-                    },
-                    fragment: Some(wgpu::FragmentState {
-                        module: &shader,
-                        entry_point: "fs_main",
-                        targets: &[Some(wgpu::ColorTargetState {
-                            format: context.surface_config.borrow().format,
-                            blend: Some(wgpu::BlendState::ALPHA_BLENDING),
-                            write_mask: wgpu::ColorWrites::ALL,
-                        })],
-                    }),
-                    primitive: wgpu::PrimitiveState {
-                        topology: wgpu::PrimitiveTopology::TriangleList,
-                        strip_index_format: None,
-                        front_face: wgpu::FrontFace::Ccw,
-                        cull_mode: None,
-                        polygon_mode: wgpu::PolygonMode::Fill,
-                        unclipped_depth: false,
-                        conservative: false,
-                    },
-                    depth_stencil: Some(wgpu::DepthStencilState {
-                        format: DepthBuffer::FORMAT,
-                        depth_write_enabled: false,
-                        depth_compare: wgpu::CompareFunction::Less,
-                        stencil: wgpu::StencilState::default(),
-                        bias: wgpu::DepthBiasState::default(),
-                    }),
-                    multisample: wgpu::MultisampleState::default(),
-                    multiview: None,
-                });
-
-        WaterPipeline { render_pipeline }
-    }
-
-    pub fn draw<'a>(
-        &'a self,
-        render_pass: &mut wgpu::RenderPass<'a>,
-        camera: &'a Camera,
-        chunks: impl Iterator<Item = &'a ChunkGraphics>,
-        sky: &'a Sky,
-        texture: &'a Texture,
-    ) {
-        render_pass.set_pipeline(&self.render_pipeline);
-        render_pass.set_bind_group(0, camera.get_bind_group(), &[]);
-        render_pass.set_bind_group(1, sky.get_bind_group(), &[]);
-        render_pass.set_bind_group(2, texture.get_bind_group(), &[]);
+        render_pass.set_pipeline(&self.water_pipeline);
 
         for chunk in chunks {
             render_pass.set_bind_group(3, chunk.offset.get_bind_group(), &[]);
@@ -253,4 +104,70 @@ impl WaterPipeline {
             render_pass.draw_indexed(0..chunk.water_mesh.index_count, 0, 0..1);
         }
     }
+}
+
+struct WorldPipelineDesc<'a> {
+    label: &'static str,
+    blend: wgpu::BlendState,
+    cull_mode: Option<wgpu::Face>,
+    depth_write_enabled: bool,
+    shader: wgpu::ShaderModuleDescriptor<'a>,
+}
+
+fn create_world_pipeline(context: &Context, desc: WorldPipelineDesc) -> wgpu::RenderPipeline {
+    let bind_group_layouts = &[
+        &Camera::create_bind_group_layout(context),
+        &Sky::create_bind_group_layout(context),
+        &Texture::create_bind_group_layout(context),
+        &Uniform::<Vector3<f32>>::create_bind_group_layout(context),
+    ];
+
+    let layout = context
+        .device
+        .create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+            label: Some(&format!("{} Layout", desc.label)),
+            bind_group_layouts,
+            push_constant_ranges: &[],
+        });
+
+    let shader = context.device.create_shader_module(desc.shader);
+
+    context
+        .device
+        .create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            label: Some(desc.label),
+            layout: Some(&layout),
+            vertex: wgpu::VertexState {
+                module: &shader,
+                entry_point: "vs_main",
+                buffers: &[Vertex::BUFFER_LAYOUT],
+            },
+            fragment: Some(wgpu::FragmentState {
+                module: &shader,
+                entry_point: "fs_main",
+                targets: &[Some(wgpu::ColorTargetState {
+                    format: context.surface_config.borrow().format,
+                    blend: Some(desc.blend),
+                    write_mask: wgpu::ColorWrites::ALL,
+                })],
+            }),
+            primitive: wgpu::PrimitiveState {
+                topology: wgpu::PrimitiveTopology::TriangleList,
+                strip_index_format: None,
+                front_face: wgpu::FrontFace::Ccw,
+                cull_mode: desc.cull_mode,
+                polygon_mode: wgpu::PolygonMode::Fill,
+                unclipped_depth: false,
+                conservative: false,
+            },
+            depth_stencil: Some(wgpu::DepthStencilState {
+                format: DepthBuffer::FORMAT,
+                depth_write_enabled: desc.depth_write_enabled,
+                depth_compare: wgpu::CompareFunction::Less,
+                stencil: wgpu::StencilState::default(),
+                bias: wgpu::DepthBiasState::default(),
+            }),
+            multisample: wgpu::MultisampleState::default(),
+            multiview: None,
+        })
 }
